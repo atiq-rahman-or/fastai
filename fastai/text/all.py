f6f19a71af7abf70165f5de77af9532da839e609
root <root@ip-172-31-71-34.ec2.internal>
2022-05-17 09:53:15 +0000

    Fix Avoid Wildcard Imports
    In file: all.py, a module is imported using the wildcard symbol. This will import public name from this module and can create conflicts in the local namespace. iCR suggested that wildcard imports should be avoided.

--- fastai/text/all.py
+++ fastai/text/all.py
@@ -1,6 +1,12 @@
-from ..basics import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
+    from ..basics import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
 from ..callback.all import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
 from .core import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
 from .data import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
 from .models import *
+# OpenRefactory Warning: Importing every public name using star import should be avoided.
 from .learner import *
